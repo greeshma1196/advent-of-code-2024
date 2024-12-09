01ec10b426565eb900e4a5a9030864d93bf0a9ef
Greeshma Ann Mathew <greeshma.mathew@gmail.com>
2024-12-09 14:59:59 +0100

    completed day 6 of aoc

---
+++ day6/day6.go
@@ -0,0 +1,283 @@
+package day6
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+func ProcessAOCDay6_1(name string) error {
+	input := readFile(name)
+
+	res := stepCounter(input)
+
+	fmt.Printf("Result day 5 part 1: %d\n", res)
+
+	return nil
+}
+
+func ProcessAOCDay6_2(name string) error {
+	input := readFile(name)
+
+	direction, r, c := getStartPos(input)
+
+	rows := len(input)
+	cols := len(input[0])
+
+	obsCount := 0
+
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			if i == r && j == c {
+				continue
+			}
+			if input[i][j] == "." {
+				input[i][j] = "#"
+				if isLoopPresent(input, direction, r, c) {
+					obsCount += 1
+				}
+				input[i][j] = "."
+			}
+		}
+	}
+
+	fmt.Printf("Result day 5 part 2: %d\n", obsCount)
+
+	return nil
+}
+
+func stepCounter(input [][]string) int {
+	direction_, row, col := getStartPos(input)
+
+	rows := len(input)
+	cols := len(input[0])
+
+	direction := direction_
+	inputp1 := input
+	isExit := false
+	r := row
+	c := col
+	for !isExit {
+		if direction == "UP" {
+			for r >= 0 {
+				if r == 0 && inputp1[r][c] != "#" {
+					inputp1[r][c] = "X"
+					isExit = true
+					break
+				}
+				if inputp1[r][c] == "#" {
+					direction = "RIGHT"
+					r += 1
+					c += 1
+					break
+				} else {
+					inputp1[r][c] = "X"
+					r -= 1
+				}
+			}
+		} else if direction == "RIGHT" {
+			for c < cols {
+				if c == cols-1 && inputp1[r][c] != "#" {
+					inputp1[r][c] = "X"
+					isExit = true
+					break
+				}
+				if inputp1[r][c] == "#" {
+					direction = "DOWN"
+					r += 1
+					c -= 1
+					break
+				} else {
+					inputp1[r][c] = "X"
+					c += 1
+				}
+			}
+		} else if direction == "DOWN" {
+			for r < rows {
+				if r == rows-1 && inputp1[r][c] != "#" {
+					inputp1[r][c] = "X"
+					isExit = true
+					break
+				}
+				if inputp1[r][c] == "#" {
+					direction = "LEFT"
+					r -= 1
+					c -= 1
+					break
+				} else {
+					inputp1[r][c] = "X"
+					r += 1
+				}
+			}
+		} else if direction == "LEFT" {
+			for c >= 0 {
+				if c == 0 && inputp1[r][c] != "#" {
+					inputp1[r][c] = "X"
+					isExit = true
+					break
+				}
+				if inputp1[r][c] == "#" {
+					direction = "UP"
+					r -= 1
+					c += 1
+					break
+				} else {
+					inputp1[r][c] = "X"
+					c -= 1
+				}
+			}
+		}
+		if isExit {
+			break
+		}
+	}
+
+	res := 0
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			if inputp1[i][j] == "X" {
+				res += 1
+			}
+		}
+	}
+
+	return res
+}
+
+func isLoopPresent(input [][]string, direction string, r int, c int) bool {
+
+	rows := len(input)
+	cols := len(input[0])
+
+	inputp1 := input
+	isExit := false
+	counter := 0
+	for !isExit {
+		if counter > 100000 {
+			break
+		}
+		if direction == "UP" {
+			for r >= 0 {
+				if r == 0 && inputp1[r][c] != "#" {
+					isExit = true
+					break
+				}
+				if inputp1[r][c] == "#" {
+					direction = "RIGHT"
+					r += 1
+					c += 1
+					break
+				} else {
+					r -= 1
+				}
+			}
+		} else if direction == "RIGHT" {
+			for c < cols {
+				if c == cols-1 && inputp1[r][c] != "#" {
+					isExit = true
+					break
+				}
+				if inputp1[r][c] == "#" {
+					direction = "DOWN"
+					r += 1
+					c -= 1
+					break
+				} else {
+					c += 1
+				}
+			}
+		} else if direction == "DOWN" {
+			for r < rows {
+				if r == rows-1 && inputp1[r][c] != "#" {
+					isExit = true
+					break
+				}
+				if inputp1[r][c] == "#" {
+					direction = "LEFT"
+					r -= 1
+					c -= 1
+					break
+				} else {
+					r += 1
+				}
+			}
+		} else if direction == "LEFT" {
+			for c >= 0 {
+				if c == 0 && inputp1[r][c] != "#" {
+					isExit = true
+					break
+				}
+				if inputp1[r][c] == "#" {
+					direction = "UP"
+					r -= 1
+					c += 1
+					break
+				} else {
+					c -= 1
+				}
+			}
+		}
+		if isExit {
+			return false
+		}
+		counter += 1
+	}
+
+	return true
+}
+
+func getStartPos(input [][]string) (string, int, int) {
+	direction := ""
+	r, c := 0, 0
+	for row, inp := range input {
+		for col, char := range inp {
+			if char == "^" {
+				direction = "UP"
+				input[row][col] = "X"
+				r, c = row, col
+				break
+			} else if char == ">" {
+				direction = "RIGHT"
+				input[row][col] = "X"
+				r, c = row, col
+				break
+			} else if char == "v" {
+				direction = "DOWN"
+				input[row][col] = "X"
+				r, c = row, col
+				break
+			} else if char == "<" {
+				direction = "LEFT"
+				input[row][col] = "X"
+				r, c = row, col
+				break
+			}
+		}
+	}
+
+	return direction, r, c
+}
+
+func readFile(name string) [][]string {
+	file, err := os.Open(name)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	var input [][]string
+	r := bufio.NewReader(file)
+	for {
+		line, _, err := r.ReadLine()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+		fields := strings.Split(string(line), "")
+		input = append(input, fields)
+	}
+	return input
+}
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/greeshma1196/advent-of-code-2024/day3"
 	"github.com/greeshma1196/advent-of-code-2024/day4"
 	"github.com/greeshma1196/advent-of-code-2024/day5"
+	"github.com/greeshma1196/advent-of-code-2024/day6"
 )
 
 func main() {
@@ -14,4 +15,6 @@ func main() {
 	_ = day3.ProcessAOCDay3("input-day3.txt")
 	_ = day4.ProcessAOCDay4("input-day4.txt")
 	_ = day5.ProcessAOCDay5("input-day5.txt")
+	_ = day6.ProcessAOCDay6_1("input-day6.txt")
+	_ = day6.ProcessAOCDay6_2("input-day6.txt")
 }
